572fb2dda9b3b29560299375f9330b68a4549ae3
Junheng Wang <wangj14@rose-hulman.edu>
2020-04-01 13:13:56 -0400

    added and tested on ddpg example

--- examples/ddpg_example.py
+++ examples/ddpg_example.py
@@ -1,38 +1,23 @@
 
-"""
-Runs one instance of the Atari environment and optimizes using DQN algorithm.
-Can use a GPU for the agent (applies to both sample and train). No parallelism
-employed, so everything happens in one python process; can be easier to debug.
-
-The kwarg snapshot_mode="last" to logger context will save the latest model at
-every log point (see inside the logger for other options).
-
-In viskit, whatever (nested) key-value pairs appear in config will become plottable
-keys for showing several experiments.  If you need to add more after an experiment, 
-use rlpyt.utils.logging.context.add_exp_param().
-
-"""
-
-from rlpyt.samplers.serial.sampler import SerialSampler
-from rlpyt.envs.atari.atari_env import AtariEnv, AtariTrajInfo
 from rlpyt.algos.qpg.ddpg import DDPG
 from rlpyt.agents.qpg.ddpg_agent import DdpgAgent
+from rlpyt.samplers.serial.sampler import SerialSampler
+from rlpyt.envs.gym import make as gym_make
 from rlpyt.runners.minibatch_rl import MinibatchRlEval
 from rlpyt.utils.logging.context import logger_context
 
 
-def build_and_train(game="pong", run_ID=0, cuda_idx=None):
+def build_and_train(env_id="Hopper-v3", run_ID=0, cuda_idx=None):
     sampler = SerialSampler(
-        EnvCls=AtariEnv,
-        TrajInfoCls=AtariTrajInfo,  # default traj info + GameScore
-        env_kwargs=dict(game=game),
-        eval_env_kwargs=dict(game=game),
-        batch_T=4,  # Four time-steps per sampler iteration.
-        batch_B=1,
+        EnvCls=gym_make,
+        env_kwargs=dict(id=env_id),
+        eval_env_kwargs=dict(id=env_id),
+        batch_T=1,  # One time-step per sampler iteration.
+        batch_B=1,  # One environment (i.e. sampler Batch dimension).
         max_decorrelation_steps=0,
         eval_n_envs=10,
-        eval_max_steps=int(10e3),
-        eval_max_trajectories=5,
+        eval_max_steps=int(51e3),
+        eval_max_trajectories=50,
     )
     algo = DDPG()  # Run with defaults.
     agent = DdpgAgent()
@@ -40,26 +25,29 @@ def build_and_train(game="pong", run_ID=0, cuda_idx=None):
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=50e6,
-        log_interval_steps=1e3,
+        n_steps=1e6,
+        log_interval_steps=1e4,
         affinity=dict(cuda_idx=cuda_idx),
     )
-    config = dict(game=game)
-    name = "ddpg_" + game
+    config = dict(env_id=env_id)
+    name = "ddpg_" + env_id
     log_dir = "ddpg_example"
-    with logger_context(log_dir, run_ID, name, config, snapshot_mode="last"):
+    with logger_context(log_dir, run_ID, name, config):
         runner.train()
+
 
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--game', help='Atari game', default='pong')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--env_id', help='environment ID',
+                        default='InvertedPendulumBulletEnv-v0')
     parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
     parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
     args = parser.parse_args()
     build_and_train(
-        game=args.game,
+        env_id=args.env_id,
         run_ID=args.run_ID,
         cuda_idx=args.cuda_idx,
     )
